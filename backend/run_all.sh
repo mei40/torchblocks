8909eb47988cb8d6115c8550c6ba4bdb85ed9d6d
mei40 <mei40@purdue.edu>
2025-03-25 12:13:35 -0400

    save frontend result location

--- backend/run_all.sh
+++ backend/run_all.sh
@@ -1,5 +1,5 @@
 cd ../
 #python3 backend/antlr/compile_main.py frontend/build/model.json
 mkdir -p backend/local/build
-python3 backend/antlr/compile_main.py backend/antlr/tests/inputs/input1.json
+python3 backend/antlr/compile_main.py backend/antlr/tests/inputs/input1.json # frontend/build/model.json
 (python3 backend/local/run_main.py) &> backend/local/build/model_run.log
