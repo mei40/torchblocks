70515e9b2f2b1cdc21b9f6f5c537b5644da2375d
smshaz <62960855+smshaz@users.noreply.github.com>
2025-04-26 18:17:38 -0400

    for backend run_all shell file fix

--- backend/run_all.sh
+++ backend/run_all.sh
@@ -3,7 +3,7 @@ cd ../
 mkdir -p backend/local/build
 (echo Starting Code Generation...) > backend/local/build/model_run.log
 python3 backend/antlr/compile_main.py frontend/build/model.json
-(echo Code Generation Finished!) >> backend/local/build/model_run.log
+(echo Code Generation Finished!) >> backend/local/build/model_run.log 
 (echo Starting Model Training...) >> backend/local/build/model_run.log
 (python3 backend/local/run_main.py 3) >> backend/local/build/model_run.log 2>&1
 (echo Model Training Finished! Please check the evaluation results. ) >> backend/local/build/model_run.log
