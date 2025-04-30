6d542056758a1d0d7bab6186ab1f693ab2cf23d1
mei40 <mei40@purdue.edu>
2025-04-30 12:37:02 -0400

    fix google + shorten training

--- backend/run_all.sh
+++ backend/run_all.sh
@@ -6,5 +6,5 @@ mkdir -p backend/antlr/build
 (python3 backend/antlr/compile_main.py frontend/build/model.json) >> backend/local/build/model_run.log 2>&1
 (echo Code Generation Finished!) >> backend/local/build/model_run.log 
 (echo Starting Model Training...) >> backend/local/build/model_run.log
-(python3 backend/local/run_main.py 3) >> backend/local/build/model_run.log 2>&1
+(python3 backend/local/run_main.py 2) >> backend/local/build/model_run.log 2>&1
 (echo Model Training Finished! Please check the evaluation results. ) >> backend/local/build/model_run.log
